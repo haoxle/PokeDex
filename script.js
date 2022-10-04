15c6ffc02ddcf426e45ba6a4c9c1b8b186ea4ed6
haoxle <hao-le@live.co.uk>
2022-10-04 16:43:43 +0100

    removed section

--- script.js
+++ script.js
@@ -39,16 +39,3 @@ const findPokemon = (e) => {
   });
 };
 enterStuff.addEventListener("submit", findPokemon);
-
-// pokemonArray.forEach((pokemon) => {
-//   const separateTypes = pokemon.types.join(" & ");
-//   const capitalName = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-//   container.innerHTML += `<div class="card">
-//   <img class="card__image"src="${pokemon.sprite}" alt="${pokemon.name}">
-//   <div class="card__content">
-//   <h1 class="card__heading">${capitalName}
-//   </h1>
-//   <p class="card__text">${capitalName} (#${pokemon.id}) is a ${separateTypes} type pokemon.</p>
-//   </div>
-//   </div>`;
-// });
